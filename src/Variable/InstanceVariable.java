edaf3261816c61e95688634b162d87c07bccdb17
kkm4512 <nayoun440@naver.com>
2024-08-11 01:01:53 +0900

    인스턴스,지역변수 1. 인스턴스 변수 : 초기값 셋팅 o 2. 지역 변수 : 초기값 셋팅 x

---
+++ src/Variable/InstanceVariable.java
@@ -0,0 +1,8 @@
+package Variable;
+
+public class InstanceVariable {
+    static int score;
+    public static void main(String[] args) {
+        System.out.println(score + 1);
+    }
+}
---
+++ src/Variable/LocalVariable.java
@@ -0,0 +1,8 @@
+package Variable;
+
+public class LocalVariable {
+    public static void main(String[] args) {
+        int score;
+        System.out.println(score+1);
+    }
+}
