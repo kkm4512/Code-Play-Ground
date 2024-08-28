634d94eeccc4ac748ab996235ef3567fa410d8b1
kkm4512 <nayoun440@naver.com>
2024-08-28 13:46:08 +0900

    DateFormatTest

---
+++ src/DateForamt/Main.java
@@ -0,0 +1,15 @@
+package DateForamt;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
+        //원하는 데이터 포맷 지정
+        String strNowDate = simpleDateFormat.format(date);
+        //지정한 포맷으로 변환
+        System.out.println("포맷 지정 후 : " + strNowDate);
+    }
+}
---
+++ src/Final/Main.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 1, y = 2, z=3;
+
+        if (x+y == 2) System.out.println(" 정답 !");
+        if (x+y == 2) System.out.println("111"); System.out.println(" 정답 !");
+    }
+}
